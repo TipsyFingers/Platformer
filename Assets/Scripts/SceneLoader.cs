b3b66b54f11ea6e784699a76f082b573e2d07f1b
Kruno <kruno_a@hotmail.com>
2018-07-30 23:06:58 +0200

    - created project

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public static void LoadMainMenuScreen()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public static void LoadLevel_1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public static void LoadGameOverScreen()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public static void LoadWinScreen()
+    {
+        SceneManager.LoadScene("Win");
+    }
+}
